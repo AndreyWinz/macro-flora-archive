250de013d50ef91bc481a8ffc56c542820899ae0
Andrey_Winz <andrey.b.kopilevich@gmail.com>
2026-04-12 22:05:18 +0200

    Create main.py

---
+++ HEIC to PNG Converter/main.py
@@ -0,0 +1,28 @@
+import os
+from PIL import Image
+import pillow_heif
+
+# set the directory path containing the HEIC files
+directory = 'heic_img'
+
+# loop through all files in the directory
+for filename in os.listdir(directory):
+     # check if the file is in HEIC format
+    if filename.lower().endswith(".heic"):
+        # create an Image object from the HEIC file
+        filepath = os.path.join(directory, filename)
+        print("Converting:", filepath)
+        heif_file = pillow_heif.read_heif(filepath)
+        image = Image.frombytes(
+            heif_file.mode,
+            heif_file.size,
+            heif_file.data,
+            "raw",
+        )
+
+        # create a new filename for the PNG file
+        new_filename = os.path.splitext(filename)[0] + ".png"
+        new_filepath = os.path.join(directory, new_filename)
+
+        image.save(new_filepath, format("png"))
+
